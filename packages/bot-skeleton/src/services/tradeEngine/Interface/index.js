28ae5e25a95deb4820703e5539e4e6a531a90643
Joseph Mumo <72470955+tombolo@users.noreply.github.com>
2026-04-06 20:24:39 +0300

    Enhance alert method with stop loss detection
    Add stop loss alert functionality to alert method.

--- packages/bot-skeleton/src/services/tradeEngine/Interface/index.js
+++ packages/bot-skeleton/src/services/tradeEngine/Interface/index.js
@@ -18,13 +18,23 @@ const Interface = $scope => {
     const tradeEngine = new TradeEngine($scope);
     const { observer } = $scope;
     const getInterface = () => {
+        const STOP_LOSS_ALERT_PATTERN = /\bstop\s*loss\b.*\b(hit|reached)\b/i;
+        const STOP_LOSS_ALERT_MESSAGE = 'Stop loss hit. Bot stopped.';
+
         return {
             ...getBotInterface(tradeEngine),
             ...getToolsInterface(tradeEngine),
             getTicksInterface: getTicksInterface(tradeEngine),
             watch: (...args) => tradeEngine.watch(...args),
             sleep: (...args) => sleep(observer, ...args),
-            alert: (...args) => alert(...args), // eslint-disable-line no-alert
+            alert: (...args) => {
+                if (typeof args?.[0] === 'string' && STOP_LOSS_ALERT_PATTERN.test(args[0])) {
+                    // eslint-disable-next-line no-alert
+                    return alert(STOP_LOSS_ALERT_MESSAGE);
+                }
+                // eslint-disable-next-line no-alert
+                return alert(...args);
+            },
             prompt: (...args) => prompt(...args), // eslint-disable-line no-alert
             console: {
                 log(...args) {
